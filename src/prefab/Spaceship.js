5f70ddb3e87fe17718db5a079d89746d6aa340b4
dinoplane <isacsac2017@gmail.com>
2022-04-06 15:55:37 -0700

    collision

--- src/prefab/Spaceship.js
+++ src/prefab/Spaceship.js
@@ -15,6 +15,10 @@ class Spaceship extends Phaser.GameObjects.Sprite {
 
         //wrap around from left edge to right edge
         if (this.x <= 0 - this.width)
-            this.x = game.config.width;
+            this.reset();
+    }
+
+    reset(){
+        this.x = game.config.width;
     }
 }
