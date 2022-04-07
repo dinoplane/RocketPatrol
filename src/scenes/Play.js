3c18ec2bb368603d8a55b582569494a6d725273b
dinoplane <isacsac2017@gmail.com>
2022-04-06 17:01:26 -0700

    fix

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -61,7 +61,8 @@ class Play extends Phaser.Scene {
             },
             fixedWidth: 100
         }
-        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);  
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, 
+                                        borderUISize + borderPadding*2, this.p1Score, scoreConfig);  
         
 
         // GAME OVER flag
@@ -70,8 +71,10 @@ class Play extends Phaser.Scene {
         // 60-second play clock
         scoreConfig.fixedWidth = 0;
         this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
-            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
-            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2, 
+                            'GAME OVER', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 
+                            'Press (R) to Restart or ← for Menu', scoreConfig).setOrigin(0.5);
             this.gameOver = true;
         }, null, this);
     }
